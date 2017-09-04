352186f4a0e1932dfd4d7477b81e5c01fd31d996
Fernando Paschualetto <paschualetto@gmail.com>
2017-09-04 19:19:30 -0300

    User rest services

--- src/main/java/com/learnrest/service/UserService.java
+++ src/main/java/com/learnrest/service/UserService.java
@@ -1,11 +1,16 @@
 package com.learnrest.service;
 
-import com.learnrest.dao.DAO;
 import com.learnrest.dao.UserDAO;
 import com.learnrest.model.User;
+import java.util.List;
 import javax.annotation.ManagedBean;
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -13,24 +18,24 @@ import javax.ws.rs.Path;
  */
 @ManagedBean
 @Path("/user")
-public class UserService extends GenericCRUDService<User> {
+public class UserService {
 
     @Inject
     private UserDAO userDAO;
 
-    public UserService() {
-        super(User.class);
+    @GET
+    @Path("/{param}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getById(@PathParam("param") Long id) {
+        Object obj = userDAO.findById(id);
+        return Response.status(200).entity(obj).build();
     }
 
-//    @GET
-//    @Path("/root")
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public User getUser() {
-//        User root = userDAO.findByUsername("root");
-//        return root;
-//    }
-    @Override
-    public DAO getDao() {
-        return userDAO;
+    @GET
+    @Path("/all")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAll() {
+        List<User> users = userDAO.findAll();
+        return Response.status(200).entity(users).build();
     }
 }
